12831b976b8dc09480842b5716c831f5e5e5509f
bhoomavsb <31505040+bhoomavsb@users.noreply.github.com>
2017-09-12 16:13:46 +0530

    bignum.c

---
+++ bignum.c
@@ -0,0 +1,14 @@
+#rag
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the number");
+scanf("%d %d %d",&a,&b,&c);
+if (a>b)
+printf("a is big");
+elseif(b>c)
+printf("b is big");
+else
+printf("c is big");
+}
